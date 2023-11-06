14de25d5363694eca76195a0c6f9ae57208b2cd2
Jonah <onahjonah@gmail.com>
2023-11-06 21:03:29 +0100

    Added partners table and fetched data to populate it

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,23 +3,24 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.db.models import Count, Sum
 from django.views import View
-from .models import Dissemination
+from .models import Dissemination, EventParticipants, Events, ExtensionAgents, Partner, Farmers
 
 
 
 @login_required
 def index(request):
     data = Dissemination.objects.all()
-    partners = Dissemination.objects.aggregate(total=Count('partner'))
+    partners = Partner.objects.all()[:10]
 
     partners_count = Dissemination.objects.values_list('partner', flat=True).distinct().count()
+    partners2_count = Partner.objects.values_list('partner', flat=True).distinct().count()
 
     male_farmers = Dissemination.objects.aggregate(total=Sum('farmers_M'))
     female_farmers = Dissemination.objects.aggregate(total=Sum('farmers_F'))
     
     return render(request, 'dashboard/index.html', 
                   {'data': data, 'partners': partners, 'male_farmers': male_farmers,
-                    'female_farmers': female_farmers, 'partners_count': partners_count})
+                    'female_farmers': female_farmers, 'partners_count': partners_count, 'partners2_count': partners2_count})
     
 
 @login_required
