69d5c9692b713fc5909c8a5c4a5aa73e42b6b5b8
Onah Jonah <onahjonah@gmail.com>
2024-02-26 23:23:26 +0100

    added the new model to forms

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -1,8 +1,14 @@
 from django import forms
-from .models import Farmers
+from .models import Farmers, NewExtensionAgents
 
 class FarmerCreationForm(forms.ModelForm):
 
     class Meta:
         model = Farmers
         fields = ['partner', 'firstname', 'lastname', 'gender', 'phone_no', 'own_phone', 'crops', 'crops_other', 'farm_area', 'area_unit', 'cassava', 'yam', 'maize', 'rice', 'sorghum']
+
+class NewExtensionAgentForm(forms.ModelForm):
+
+    class Meta:
+        model = NewExtensionAgents
+        fields = ['firstname', 'lastname', 'gender', 'phone_no', 'phone_no2', 'whatsapp', 'email', 'age', 'designation', 'type_org', 'org', 'country']
