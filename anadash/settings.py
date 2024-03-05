0c3b73a880f9fbb80bce3ec2f65b5e429d7d290f
Onah Jonah <onahjonah@gmail.com>
2024-03-05 10:10:32 +0100

    added allowed hosts and trusted origins

--- anadash/settings.py
+++ anadash/settings.py
@@ -18,7 +18,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-CSRF_TRUSTED_ORIGINS = ["https://ana.akilimo.org/"]
+CSRF_TRUSTED_ORIGINS = ["https://ana.akilimo.org", "https://*.127.0.0.1"]
 
 
 # Application definition
